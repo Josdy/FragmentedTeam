e9ffb302a09ff00f6f87816b4707f2c9f2785348
carlosmjuncoj <carlosm.junco.j@gmail.com>
2022-10-22 09:08:48 -0500

    Primera version

--- all_inclusive/lib/main.dart
+++ all_inclusive/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'PasswordScreenInFlutter.dart';
+
 void main() {
   runApp(const MaterialApp(
     title: 'Navigation Basics',
@@ -100,12 +102,12 @@ Future<void> _showSimpleDialog(BuildContext context) async {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromRGBO(250, 250, 250, 1),
                   foregroundColor: const Color.fromRGBO(105, 165, 210, 1),
-                  minimumSize: const Size(100, 45), //////// HERE
+                  minimumSize: const Size(100, 45), 
                 ),
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const ThirdRoute()),
+                    MaterialPageRoute(builder: (context) =>  MyHomePage()),
                   );
                 },
                 child: const Text('Continuar'),
@@ -130,76 +132,53 @@ Future<void> _showSimpleDialog(BuildContext context) async {
 }
 
 class ThirdRoute extends StatelessWidget {
+  
   const ThirdRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return GestureDetector( 
+       onTap: () {  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) =>  const LastRoute()),
+                  );},
+      child:Container(
         constraints: const BoxConstraints.expand(),
-        color: Colors.white,
+       
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/loading_view.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: ListView(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(100),
-              ),
-              Container(
-                color: Colors.transparent,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(8),
-                child: const CircleAvatar(
-                    backgroundImage:
-                        AssetImage("assets/images/user_profile.jpg"),
-                    radius: 20 //Text
-                    ), //CircleAvatar
-              ),
-              Container(
-                  color: Colors.transparent,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.all(2),
-                  child: RichText(
-                    text: const TextSpan(
-                        text: 'last site',
-                        style: TextStyle(color: Colors.white, fontSize: 15)),
-                  ) //CircleAvatar
-                  ),
-              Container(
-                  color: Colors.transparent,
-                  alignment: Alignment.center,
-                  child: RichText(
-                    text: const TextSpan(
-                        text: 'Cambiar usuario',
-                        style: TextStyle(
-                            color: Color.fromRGBO(77, 123, 161, 1),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold)),
-                  ) //CircleAvatar
-                  ),
-              Container(
-                color: Colors.transparent,
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(41, 134, 203, 1),
-                    foregroundColor: Colors.white,
-                    minimumSize: const Size(100, 45), //////// HERE
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const FirstRoute()),
-                    );
-                  },
-                  child: const Text('Iniciar sesión'),
-                ),
-              ),
-            ],
+     ) ));
+  }
+}
+class LastRoute extends StatelessWidget {
+  
+  const LastRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector( 
+       onTap: () {  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) =>  const FirstRoute()),
+                  );},
+      child:Container(
+        constraints: const BoxConstraints.expand(),
+       
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/dashboard.png"),
+            fit: BoxFit.cover,
           ),
-        ));
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+     ) ));
   }
 }
 
@@ -283,5 +262,47 @@ class SecondRoute extends StatelessWidget {
             ],
           ),
         ));
+  }
+}
+
+class ButtonForHome extends StatelessWidget {
+  final String label;
+  final int id; // this will save your day
+
+  // @required will not let user to skip the specified key to be left null
+  ButtonForHome({required this.label, required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(41, 134, 203, 1),
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(100, 45), //////// HERE
+                ),
+                child: Text(this.label),
+                onPressed: () => print(
+                    this.id) // Here you print your id using your button only
+                )));
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+
+    return PasswordView();
   }
 }
